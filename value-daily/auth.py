417b2b86eb8fc22d5aa2b5086bad043263f1664a
Mpallways <114129851+Mpallways@users.noreply.github.com>
2026-06-05 12:35:07 +0530

    Update auth.py

--- value-daily/auth.py
+++ value-daily/auth.py
@@ -1,8 +1,10 @@
 from growwapi import GrowwAPI
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except:
+    pass
 
 
 def get_client():
