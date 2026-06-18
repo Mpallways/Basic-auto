388a9585256d94d5e6eab6fa4f88548130041921
Mpallways <114129851+Mpallways@users.noreply.github.com>
2026-06-18 19:44:47 +0530

    Updating 2nd changes

--- value-daily/portfolio.py
+++ value-daily/portfolio.py
@@ -1,6 +1,55 @@
 import pandas as pd
-def fetch_holdings(client):
-    response = client.get_holdings_for_user()
-    holdings = response["holdings"]
-    df = pd.DataFrame(holdings)
+import json
+import os
+import gspread
+from google.oauth2.service_account import Credentials
+
+
+def fetch_holdings():
+
+    scopes = [
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive"
+    ]
+
+    if os.path.exists("portfoliotracker-june-2927b58e2103.json"):
+        creds = Credentials.from_service_account_file(
+        "portfoliotracker-june-2927b58e2103.json",
+        scopes=scopes
+    )
+    else:
+        service_account_info = json.loads(
+            os.environ["GOOGLE_SERVICE_ACCOUNT_JSON"]
+        )
+
+        creds = Credentials.from_service_account_info(
+            service_account_info,
+            scopes=scopes
+        )
+
+    client = gspread.authorize(creds)
+
+    sheet = client.open_by_key(
+        "1jUe2hJwaFAbVMycy6Rgpa2FLs0ZMZaPR2klXnLAHkMQ"
+    ).sheet1
+
+    records = sheet.get_all_records()
+
+    df = pd.DataFrame(records)
+
+    df["Qty"] = (
+        df["Qty"]
+        .astype(str)
+        .str.replace(",", "", regex=False)
+        .astype(float)
+    )
+
+    df["Avg Price"] = (
+        df["Avg Price"]
+        .astype(str)
+        .str.replace("₹", "", regex=False)
+        .str.replace(",", "", regex=False)
+        .astype(float)
+    )
+
     return df
